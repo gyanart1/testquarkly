b10765f4d4b0e9700b0cab679529b8cb75d23a2a
Gyaneshwar Mule <47977936+gyanart1@users.noreply.github.com>
2021-01-30 13:26:31 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6015101d977a55001e577ded",
+				"6015101d977a55001e577def"
+			]
+		},
+		"6015101d977a55001e577ded": {
+			"id": "6015101d977a55001e577ded",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6015101d977a55001e577def": {
+			"id": "6015101d977a55001e577def",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
